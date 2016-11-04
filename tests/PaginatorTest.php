f1ea8237e8526e89f3b239733619899bf7a9cd01
Nico Amarilla <kosinix@codefleet.net>
2016-11-04 15:13:59 +0800

    Update tests

--- tests/PaginatorTest.php
+++ tests/PaginatorTest.php
@@ -6,11 +6,12 @@ class PaginatorTest extends PHPUnit_Framework_TestCase
     public function testOnZeroTotal()
     {
         $total = 0;
-        $paginator = new Paginator($total);
+        $current_page = 1;
+        $per_page = 10;
+        $paginator = new Paginator($total, $current_page, $per_page);
         
-        $this->assertEquals(0, $paginator->get_start_index());
-        $this->assertEquals(0, $paginator->get_end_index(), 'end_index should be zero if total is zero');
-        $this->assertEquals(0, $paginator->get_previous_page(), 'previous_page should be zero');
+        $this->assertEquals(0, $paginator->getStartIndex());
+        $this->assertEquals(0, $paginator->getEndIndex(), 'end_index should be zero if total is zero');
     }
     
     
@@ -21,35 +22,9 @@ class PaginatorTest extends PHPUnit_Framework_TestCase
         $current_page = 2;
         $paginator = new Paginator($total, $current_page, $per_page);
         
-        $this->assertEquals(10, $paginator->get_start_index());
-        $this->assertEquals(19, $paginator->get_end_index());
-        $this->assertEquals(1, $paginator->get_previous_page());
-        $this->assertEquals(3, $paginator->get_next_page());
-    }
-    
-    public function testClassGettersCompleteness(){
-        
-        $missing_functions = array();
-        $public_functions = array();
-        
-        $reflection = new ReflectionClass('\Kosinix\Paginator');
-        
-        // Get class public functions
-        foreach( $reflection->getMethods(ReflectionMethod::IS_PUBLIC) as $method ) {
-            if($method->name !== '__construct'){
-                $public_functions[] = $method->name;
-            }
-        }
-        
-        // Generate a getter name for each class variables and test existence
-        foreach( $reflection->getProperties() as $field ) {
-            $getter_name = 'get_'.$field->name; // Format: get_[method name]
-            if(!in_array($getter_name, $public_functions)){
-                $missing_functions[] = $getter_name;
-            }
-        }
-        sort($missing_functions); // Sort alphabetically
-        
-        $this->assertCount(0, $missing_functions, 'Missing getter functions from class Paginator: '.print_r($missing_functions, true));
+        $this->assertEquals(10, $paginator->getStartIndex());
+        $this->assertEquals(19, $paginator->getEndIndex());
+        $this->assertEquals(1, $paginator->getCurrentPage()-1);
+        $this->assertEquals(3, $paginator->getCurrentPage()+1);
     }
 }
